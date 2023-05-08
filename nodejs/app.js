0ad07af206e3f5953e28011061e0ebfa8beb373c
Deepak Sharma <112716576+uddisharma@users.noreply.github.com>
2023-05-08 21:53:57 +0530

    Add files via upload

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,11 +1,21 @@
 const fs = require("fs");
 fs.writeFileSync("hello.txt", "hello world ");
-const http = require('http');
+const http = require("http");
 
 const port = 4000;
 
 const server = http.createServer((req, res) => {
-  res.end('Hello, my name is Deepak Sharma');
+  // res.end('Hello, my name is Deepak Sharma');
+
+  if ((req.url = "/home")) {
+    res.end("hello this is home page");
+  } else if ((req.url = "/about")) {
+    res.end("hello this is about page");
+  } else if ((req.url = "/contact")) {
+    res.end("hello this is contact page");
+  }else{
+    res.end('page not found');
+  }
 });
 
 server.listen(port, () => {
