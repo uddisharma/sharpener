6248c754bd4e78c678a31a8d312efb098aa7b5ae
Deepak Sharma <112716576+uddisharma@users.noreply.github.com>
2023-05-09 23:37:27 +0530

    Add files via upload

--- express/app.js
+++ express/app.js
@@ -1,30 +1,42 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-app.get(
-  "/",
-  (req, res, next) => {
-    console.log("this is middleware");
-    next();
-  },
-  (req, res) => {
-    res.send(
-      `<div><h1>this is home page after using the middleware</h1></div>`
-    );
-  }
-);
-app.use('/middleware', (req, res, next) =>{
-    console.log("middleware start")
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.get(
+//   "/",
+//   (req, res, next) => {
+//     console.log("this is middleware");
+//     next();
+//   },
+//   (req, res) => {
+//     res.send(
+//       `<div><h1>this is home page after using the middleware</h1></div>`
+//     );
+//   }
+// );
+// app.use('/middleware', (req, res, next) =>{
+//     console.log("middleware start")
     
-    next()
+//     next()
+// })
+// app.get('/middleware', (req, res, next) => {
+//     console.log("main funtion")
+//     next()
+// })   
+// app.use('/middleware', (req, res, next) => {
+//     console.log("end of main funtion with middleware")
+//     res.send('this is middleware page with middleware') 
+// });
+app.use('/add-products',(req,res,next)=>{
+    res.send('<form method="POST" action="/products"><input name="product" type="text"/><button type="submit">add</button></form')
 })
-app.get('/middleware', (req, res, next) => {
-    console.log("main funtion")
-    next()
-})   
-app.use('/middleware', (req, res, next) => {
-    console.log("end of main funtion with middleware")
-    res.send('this is middleware page with middleware') 
-});
+app.use('/products', (req,res)=>{
+    console.log(req.body)
+    res.redirect('/')
+})
+app.use('/',(req,res)=>{
+    res.send('this is home page')
+})
 app.listen(3000, (req, res) => {
   console.log("Listening on port", 3000);
 });
